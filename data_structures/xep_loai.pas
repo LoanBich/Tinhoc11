cbf489bf05657a7d6560843abf1426859407cbbd
LoanBich <tranbichloan25112002@gmail.com>
2023-12-10 14:28:36 +0700

    o

--- data_structures/xep_loai.pas
+++ data_structures/xep_loai.pas
@@ -6,19 +6,18 @@
         b2: tinh xep loai
 }
 
-program xeploai;
+{program xeploai;
 uses crt;
 type hs = record
-                hoten: string;
+                hoten: string[30];
                 diemtoan: real;
                 diemvan: real;
-                xeploai: string;
+                xeploai: char;
             end;
 var lop: array[1..60] of hs;
     N, i: byte;
 begin
     clrscr;
-    {nhap diem hs}
     write('so luong hoc sinh: '); readln(N);
     for i:= 1 to N do 
         begin
@@ -32,4 +31,31 @@ begin
     for i:=1 to N do
         write('hoc sinh ', i, lop[i].hoten, 'xep loai: ', lop[i].xeploai);
         readln;
+end.}
+
+program xeploai;
+uses crt;
+const max=60;
+type hocsinh = record
+                    hoten: string[30];
+                    toan, van: real;
+                    xeploai: char;
+                end;
+var 
+    lop: array[1..max] of hocsinh;
+    N, i: Byte;
+begin 
+    clrscr;
+    write('so hs: '); readln(N);
+    for i:= 1 to N do
+        begin 
+            write('nhap s lieu hs thu ', i, ': ');
+            write('ho ten: '); readln(lop[i].hoten);
+            write('diem toan: '); readln(lop[i].toan);
+            write('diem van: '); readln(lop[i].van);
+            if lop[i].toan+lop[i].van>=16 then lop[i].xeploai:= 'A'
+            else lop[i].xeploai :='B';
+        end;
+    for i:= 1 to N do writeln(lop[i].hoten, ': ', lop[i].xeploai);
 end.
+                    
